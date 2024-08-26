66a04ca597a8476a1e2414f9b5147a7486a9db92
comjun04 <comjun04@gmail.com>
2024-08-26 22:30:08 +0900

    feat: basic app with hono

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello world')
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => c.json({ hello: 'world' }))
+
+serve(app, (info) => {
+  console.log(`Listening on ${info.address}:${info.port}`)
+})
